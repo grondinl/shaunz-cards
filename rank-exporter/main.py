d10ab46e463bb1e06013f1d307781db2950fb35c
grondinl <lo.grondin@gmail.com>
2022-03-24 16:41:52 +0100

    Add metrics for cards by tier

--- rank-exporter/main.py
+++ rank-exporter/main.py
@@ -2,20 +2,24 @@ from flask import Flask
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from prometheus_client import make_wsgi_app
 from prometheus_client.core import GaugeMetricFamily, REGISTRY
-from requests import post 
+from requests import post
 
 
 class CustomCollector(object):
     def collect(self):
         metrics = {
             'points_gauge' : GaugeMetricFamily('papichoulo_cards_score','', labels=['name']),
-            'count_gauge' : GaugeMetricFamily('papichoulo_cards_count','', labels=['name'])
+            'count_gauge' : GaugeMetricFamily('papichoulo_cards_count','', labels=['name']),
+            'count_gauge_by_tier' : GaugeMetricFamily('papichoulo_cards_count_by_tier','', labels=['name', 'tier']),
         }
         res = post('https://api.cards.shaunz.fr/classement')
         classement = res.json()["classement"]
         for rank in classement: 
+            cards_by_tier = post('https://api.cards.shaunz.fr/joueur', data={'userId': rank['id']}).json()['nbCardsByTier']
             metrics['points_gauge'].add_metric([rank['name']], rank['score'])
             metrics['count_gauge'].add_metric([rank['name']], rank['cards'])
+            for tier in cards_by_tier.keys():
+                metrics['count_gauge_by_tier'].add_metric([rank['name'],tier], cards_by_tier[tier])
         for metric in metrics.keys():
             yield metrics[metric]
 
