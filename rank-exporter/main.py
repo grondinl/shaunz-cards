d95d8dd17a3fc99a04f948580f03cdfe7cd90c7e
grondinl <lo.grondin@gmail.com>
2022-03-30 16:53:46 +0200

    Add shiny count

--- rank-exporter/main.py
+++ rank-exporter/main.py
@@ -12,24 +12,51 @@ class CustomCollector(object):
             'points_gauge' : GaugeMetricFamily('papichoulo_cards_score','', labels=['name']),
             'count_gauge' : GaugeMetricFamily('papichoulo_cards_count','', labels=['name']),
             'count_gauge_by_tier' : GaugeMetricFamily('papichoulo_cards_count_by_tier','', labels=['name', 'tier']),
+            'shiny_count_gauge' : GaugeMetricFamily('papichoulo_shiny_count','', labels=['name', 'tier']),
         }
         res = post('https://api.cards.shaunz.fr/classement')
-        classement = res.json()["classement"]
+        classement = res.json()['classement']
         for rank in classement: 
+            name = rank['name']
+            joueur = post('https://api.cards.shaunz.fr/joueur', data={'userId': rank['id']}).json()
+            cards_by_tier = joueur['nbCardsByTier']
+            list_cards = joueur['listCards']
+            metrics['points_gauge'].add_metric([name], rank['score'])
+            metrics['count_gauge'].add_metric([name], rank['cards'])
+
+            for tier, count in cards_by_tier.items():
+                metrics['count_gauge_by_tier'].add_metric([name,tier], count)
+
+            has_shiny = False
+            for trophee in joueur['trophees']:
+                if trophee['name'] == 'shinny':
+                    has_shiny = trophee['value']
+
+            if has_shiny:
+                shiny_count_by_tier = {'1':0,'2':0,'3':0,'4':0,'5':0}
+                for card in list_cards:
+                    if 'shiny' in card:
+                        level = str(card['level'])
+                        shiny_count_by_tier[level] += 1
+
+                for tier, shiny_count in shiny_count_by_tier.items():
+                    if shiny_count != 0:
+                        metrics['shiny_count_gauge'].add_metric([name,tier], shiny_count)
+             
             sleep(0.1)
-            cards_by_tier = post('https://api.cards.shaunz.fr/joueur', data={'userId': rank['id']}).json()['nbCardsByTier']
-            metrics['points_gauge'].add_metric([rank['name']], rank['score'])
-            metrics['count_gauge'].add_metric([rank['name']], rank['cards'])
-            for tier in cards_by_tier.keys():
-                metrics['count_gauge_by_tier'].add_metric([rank['name'],tier], cards_by_tier[tier])
+
         for metric in metrics.keys():
             yield metrics[metric]
 
 REGISTRY.register(CustomCollector())
-# Create my app
+
 app = Flask(__name__)
 
 # Add prometheus wsgi middleware to route /metrics requests
 app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
     '/metrics': make_wsgi_app()
 })
+
+@app.route("/")
+def index():
+    return "ok"
