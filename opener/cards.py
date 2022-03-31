f676151a65cf216ed046bb75805c0a5ec98354d2
grondinl <lo.grondin@gmail.com>
2022-03-31 12:21:22 +0200

    Trade token 2 and 3

--- opener/cards.py
+++ opener/cards.py
@@ -12,25 +12,33 @@ def make_call(api_url, data):
 def login(base_api_url, data):
     resp = make_call(base_api_url + '/login', data)
     if resp.status_code != 200:
-        print("Logging failed")
+        print('Logging failed')
         return {}
-    print("Logged as {}".format(resp.json()['name']))
+    print('Logged as {}'.format(resp.json()['name']))
     return resp.json()['tokens']
 
 def get_cards(base_api_url, data, card_token):
-    print("Get card with token {}".format(card_token), end=' ')
+    print('Get card with token {}'.format(card_token), end=' ')
     data['token'] = card_token
     resp = make_call(base_api_url + '/getCard', data)
     if resp.status_code != 200:
-        print("failed : {}".format(resp.text))
-    print("success")
+        print('failed : {}'.format(resp.text))
+    print('success')
+
+def trade_token(base_api_url, data, card_token):
+    print('Trade token {}'.format(card_token), end=' ')
+    data['token'] = card_token
+    resp = make_call(base_api_url + '/tradeToken', data)
+    if resp.status_code != 200:
+        print('failed : {}'.format(resp.text))
+    print('success')
 
 def gamble_pack(base_api_url, data):
-    print("Gambling Pack", end=' ')
+    print('Gambling Pack', end=' ')
     resp = make_call(base_api_url + '/gamblePack', data)
     if resp.status_code != 200:
-        print("failed : {}".format(resp.text))
-    print("success")
+        print('failed : {}'.format(resp.text))
+    print('success')
 
 @click.command()
 @click.option('--access-token', required=True, envvar='ACCESS_TOKEN')
@@ -38,17 +46,22 @@ def gamble_pack(base_api_url, data):
 @click.option('--base-api-url', default='https://api.cards.shaunz.fr')
 def main(access_token, user_id, base_api_url):
     data = {'accessToken': access_token, 'userId' : user_id}
-    tokens = login(base_api_url, data)
+    login_resp = login(base_api_url, data)
+    tokens = login_resp['tokens']
     tokens_list = list(tokens.keys())
     for token_key in reversed(tokens_list):
         token_count = tokens[token_key] 
         for i in range(0,token_count):
-            if token_key == "6":
+            if token_key == '6':
                 gamble_pack(base_api_url, data)
                 if i == token_count - 1:
                     tokens = login(base_api_url, data)
                 continue
-            get_cards(base_api_url, data, token_key)
+            if token_key == '2' or token_key == '3':
+                trade_token(base_api_url, data, token_key)
+                continue
+            if login_resp['hasAllCardsByTier'][token_key] == False:
+                get_cards(base_api_url, data, token_key)
 
 if __name__ == '__main__':
     main()
