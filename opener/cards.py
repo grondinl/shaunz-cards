3b0bfd6e53129c6c0cdd79fd904d7d3061bde27a
grondinl <lo.grondin@gmail.com>
2022-05-05 11:40:52 +0200

    Fix empty token to trade and loging

--- opener/cards.py
+++ opener/cards.py
@@ -52,31 +52,35 @@ def validate_token_to_trade(ctx, param, value):
     if isinstance(value, list):
         return value
     try:
-        tokens = value.split(",")
+        if value == '':
+            return []
+        tokens = value.split(',')
         for token in tokens:
             if int(token) < 0 or  int(token) > 5:
-                raise ValueError
+                raise ValueError("{} has invalid format must be comma separated numbers between 1 to 5 ".format(value))
         return tokens
-    except ValueError:
-        raise click.BadParameter("format must be number between 1 to 5 comma separated")
+    except ValueError as e:
+        raise click.BadParameter(e)
 
 @click.command()
-@click.option('--access-token', required=True, envvar='ACCESS_TOKEN')
-@click.option('--user-id', required=True, envvar='USER_ID')
+@click.option('--access-token', required=False, envvar='ACCESS_TOKEN')
+@click.option('--user-id', required=False, envvar='USER_ID')
 @click.option('--base-api-url', default='https://api.cards.shaunz.fr')
-@click.option('--token-to-trade', envvar='TOKEN_TO_TRADE', default='2,3', callback=validate_token_to_trade)
+@click.option('--token-to-trade', envvar='TOKEN_TO_TRADE', default='', callback=validate_token_to_trade)
 def main(access_token, user_id, base_api_url, token_to_trade):
     data = {'accessToken': access_token, 'userId' : user_id}
     login_resp = login(base_api_url, data)
     tokens = login_resp['tokens']
-    tokens_list = ['6','1','2','3','4','5',]
+    tokens_list = ['6','1','2','3','4','5']
     for token_key in tokens_list:
         token_count = tokens[token_key] 
         if token_key == '6':
             login_resp = multi_gamble_pack(base_api_url, data, token_count)
+            tokens = login_resp['tokens']
             continue
         if token_key in token_to_trade or login_resp['hasAllCardsByTier'][token_key]:
-            multi_trade_token(base_api_url, data, token_key, token_count)
+            login_resp = multi_trade_token(base_api_url, data, token_key, token_count)
+            tokens = login_resp['tokens']
             continue
         multi_get_cards(base_api_url, data, token_key, token_count)
 
